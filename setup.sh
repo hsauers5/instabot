c8b68e66b3cf32390ad849dc8761afc1493c8b4e
Harry Sauers <hsauers5@gmail.com>
2019-08-07 20:59:53 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt install -y python3-pip
 sudo pip3 install selenium
 sudo pip3 install xvfbwrapper
 sudo apt install -y firefox
-wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
+sudo wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
 sudo tar xzf geckodriver-v0.24.0-linux64.tar.gz
 sudo chmod +x geckodriver
 sudo mv geckodriver /usr/local/bin/geckodriver
